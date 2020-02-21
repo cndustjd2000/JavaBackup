c5894395875fc647344b2acd6aec55893a0301ac
HUu-11 <HUu-11@HUu-11-PC>
2020-02-21 16:59:11 +0900

    adfadsfafs

---
+++ src/com/human/ex/example.java
@@ -0,0 +1,18 @@
+package com.human.ex;
+
+public class example {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc=new java.util.Scanner(System.in);
+		
+		//1번 문제
+		
+		
+		//2번 문제
+		
+		
+		//3번 문제
+
+	}
+
+}
